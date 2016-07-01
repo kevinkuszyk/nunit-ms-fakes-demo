ab7323248a4f63356fcf7f871673b37f5ef14c08
Kevin Kuszyk <kevin@kevinkuszyk.com>
2016-07-01 12:31:03 +0100

    Initial commit

---
+++ src/TestProject/TestClass.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.QualityTools.Testing.Fakes;
+using NUnit.Framework;
+
+namespace TestProject
+{
+    [TestFixture]
+    public class TestClass
+    {
+        [Test]
+        public void Test()
+        {
+            // Arrange
+            using (var context = ShimsContext.Create()) // UnitTestIsolationException is thrown here.
+            {
+                
+            }
+
+            // Act
+
+            // Assert
+            Assert.That(true, Is.True);
+        }
+    }
+}
